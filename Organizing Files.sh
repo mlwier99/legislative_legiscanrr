36994f14672fd468839277c8d0f0a58615f2d597
Michelle Wier <48560157+mlwier99@users.noreply.github.com>
2021-04-26 15:38:40 -0400

    Update Organizing Files.sh
    Changed last-run state list to full states list

--- Organizing Files.sh
+++ Organizing Files.sh
@@ -34,7 +34,9 @@ do
   done
 done 
 
-StateArray=("HI" "CA")
+StateArray=("AL" "AK" "AZ" "AR" "CA" "CO" "CT" "DE" "FL" "GA" "HI" "ID" "IL" "IN" "IA" "KS" "KY" "LA" 
+  "ME" "MD" "MA" "MI" "MN" "MS" "MO" "MT" "NE" "NV" "NH" "NJ" "NM" "NY" "NC" "ND" "OH" "OK" "OR" "PA" 
+  "RI" "SC" "SD" "TN" "TX" "UT" "VT" "VA" "WA" "WV" "WI" "WY")
 #Run AFTER parsing bills.
 
 FileArray=("bill" "people" "vote")
