cdf201e5c445a4c4b1673fd1ae317cb472502a9d
Michelle Wier <48560157+mlwier99@users.noreply.github.com>
2021-04-26 15:34:16 -0400

    Update Organizing Files.sh
    full state list

--- Organizing Files.sh
+++ Organizing Files.sh
@@ -7,9 +7,9 @@ ls
 
 
 #All States
-StatesArray=("AL" "AK" "AZ" "AR" "CA" "CO" "CT" "DE" "FL" "GA" "HI" "ID" "IL" "IN" "IA" "KS" "KY" "LA"
-  "ME" "MD" "MA" "MI" "MN" "MS" "MO" "MT" "NE" "NV" "NH" "NJ" "NM" "NY" "NC" "ND" "OH" "OK" "OR" "PA" "RI"
-  "SC" "SD" "TN" "TX" "UT" "VT" "VA" "WA" "WV" "WI" "WY")
+StatesArray=("AL" "AK" "AZ" "AR" "CA" "CO" "CT" "DE" "FL" "GA" "HI" "ID" "IL" "IN" "IA" "KS" "KY" "LA" 
+  "ME" "MD" "MA" "MI" "MN" "MS" "MO" "MT" "NE" "NV" "NH" "NJ" "NM" "NY" "NC" "ND" "OH" "OK" "OR" "PA" 
+  "RI" "SC" "SD" "TN" "TX" "UT" "VT" "VA" "WA" "WV" "WI" "WY")
 
 StateArray=("") 
 
